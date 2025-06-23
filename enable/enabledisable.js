8ab58fd428acee27965d16fadd7b9ae35f27c553
Ash <ashleypan9901@gmail.com>
2025-06-23 11:18:07 -0400

    Changes made

--- enable/enabledisable.js
+++ enable/enabledisable.js
@@ -1,13 +1,15 @@
 const scriptSheetURL = 'https://docs.google.com/spreadsheets/d/1lsmDvfbU9L4n6yuMZcWnH3OzVSDlq3uK3GZsN0pEmw0/edit?gid=1681209226#gid=1681209226'; //make copy, replace account name and sale name
 const scriptSheet =  SpreadsheetApp.openByUrl(scriptSheetURL);
 
+
 //DO NOT EDIT ANY CODE BELOW THIS LINE (or I'll cry)
 
 const settings = scriptSheet.getSheetByName("Settings");
-const saleLabel = scriptSheet.getRange("D11").getValue();
-const saleStart = scriptSheet.getRange("D14").getValue();
-const saleEnd = scriptSheet.getRange("D17").getValue();
-const email = scriptSheet.getRange("D20").getValue();
+
+const saleLabel = settings.getRange("D11").getValue();
+const saleStart = settings.getRange("D14").getValue();
+const saleEnd = settings.getRange("D17").getValue();
+const email = settings.getRange("D20").getValue();
 
 
 const campaignQuery = `
@@ -33,6 +35,16 @@ WHERE
     AND ad_group_ad.status != 'REMOVED' 
 `;
 
+const adGroupQuery = `
+SELECT 
+    ad_group.id, 
+    ad_group.name, 
+    ad_group.status
+FROM ad_group 
+WHERE 
+    ad_group.status != 'REMOVED'
+`;
+
 function getCurrentDate () {
     const timeZone = AdsApp.currentAccount().getTimeZone();
     const now = new Date ();
@@ -51,30 +63,52 @@ function isSaleActive (){
     } 
 }
 
-function CampaignEnablePause(campaign){
-    if (isSaleActive() && campaign.isPaused()){
-        campaign.enable();
-    } else if (!isSaleActive() && campaign.isEnabled()) {
-        campaign.pause();
-    }
-}
-
-function AdEnablePause(ad){
-    if (isSaleActive() && ad.isPaused()){
-        ad.enable();
-    } else if (!isSaleActive() && ad.isEnabled()) {
-        ad.pause();
+function toggleEnablePause(entity){
+    if (isSaleActive() && entity.isPaused()){
+        entity.enable();
+        Logger.log(`${entity.getEntityType()} with ID: ${entity.getId()} has been ENABLED.`);
+    } else if (!isSaleActive() && entity.isEnabled()){
+        entity.pause();
+        Logger.log(`${entity.getEntityType()} with ID: ${entity.getId()} has been PAUSED.`);
     }
 }
 
 function main (){
 
+    const saleLabel = settings.getRange("D11").getValue();
+
     if (!saleLabel) {
-        return "error: sales label empty, review sheet."
+        Logger.log("error: sales label empty, review sheet.");
+        return;
     }
 
-    isSaleActive();
+    const campaignIterator = AdsApp.campaigns()
+    .withCondition(`LabelNames CONTAINS_ANY ['${saleLabel}']`)
+    .get();
 
+    while (campaignIterator.hasNext ()) {
+        const campaign = campaignIterator.next();
+        toggleEnablePause(campaign);
+    }
 
+    const adIterator = AdsApp.ads()
+    .withCondition(`LabelNames CONTAINS_ANY ['${saleLabel}']`)
+    .get();
+
+    while (adIterator.hasNext()){
+        const ad = adIterator.next();
+        toggleEnablePause(ad)
+    }
+
+    const adGroupIterator = AdsApp.adGroups()
+    .withCondition(`LabelNames CONTAINS_ANY ['${saleLabel}']`)
+    .get();
+
+    while (adGroupIterator.hasNext()){
+        const adGroup = adGroupIterator.next(); 
+        toggleEnablePause(adGroup)
+    }
+
+    
 }
 
